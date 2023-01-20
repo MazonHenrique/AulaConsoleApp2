61fcd02b034b4a18679fa8648c40deaf5a87c7f5
MazonHenrique <hmazon87@yahoo.com>
2023-01-19 22:51:01 -0300

    Teste   Adicionar arquivos de projeto.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp2
     {
         static void Main(String[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Olá, Mundo!");
         }
 
     }
