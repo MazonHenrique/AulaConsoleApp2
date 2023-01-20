c17247f5897b50ba255c37d215cc0828bb68c251
cinti <cinti@DESKTOP-36Q3DG2>
2023-01-19 22:44:32 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+
+}
